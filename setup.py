fe9e27e980e5abcb9a4424145fc24706c61827d1
Vincent Driessen <vincent@3rdcloud.com>
2012-02-01 21:53:09 +0100

    Add PyPI package definition.

---
+++ setup.py
@@ -0,0 +1,73 @@
+"""
+Times is a small, minimalistic, Python library for dealing with time conversions
+to and from timezones, for once and for all.
+"""
+import os
+from setuptools import setup
+
+def get_version():
+    basedir = os.path.dirname(__file__)
+    with open(os.path.join(basedir, 'times/version.py')) as f:
+        VERSION = None
+        exec(f.read())
+        return VERSION
+    raise RuntimeError('No version info found.')
+
+setup(
+    name='times',
+    version=get_version(),
+    url='https://github.com/nvie/times/',
+    license='BSD',
+    author='Vincent Driessen',
+    author_email='vincent@3rdcloud.com',
+    description='Times is a small, minimalistic, Python library for dealing '
+           'with time conversions to and from timezones, for once and for all.',
+    long_description=__doc__,
+    packages=['times'],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=['pytz'],
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        #'Development Status :: 1 - Planning',
+        #'Development Status :: 2 - Pre-Alpha',
+        #'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        #'Development Status :: 5 - Production/Stable',
+        #'Development Status :: 6 - Mature',
+        #'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Environment :: MacOS X',
+        'Environment :: No Input/Output (Daemon)',
+        'Environment :: Other Environment',
+        'Environment :: Plugins',
+        'Environment :: Web Environment',
+        'Environment :: Win32 (MS Windows)',
+        'Framework :: Buildout',
+        'Framework :: CherryPy',
+        'Framework :: Django',
+        'Framework :: Plone',
+        'Framework :: Pylons',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Legal Industry',
+        'Intended Audience :: Manufacturing',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Office/Business',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development',
+        'Topic :: System',
+        'Topic :: Terminals',
+        'Topic :: Utilities',
+    ]
+)
